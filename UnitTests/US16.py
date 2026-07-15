88d1a6cf478334be4c0ba59c0db4a7931947e144
choudhs2 <choudhuryshadman2@gmail.com>
2026-07-14 23:24:20 -0400

    Test Cases

---
+++ UnitTests/US16.py
@@ -0,0 +1,62 @@
+import unittest
+import io
+from datetime import date
+from GEDCOMParser import *
+
+class TestForSiblingMarriageValidation(unittest.TestCase):
+
+    def test_marriage_not_siblings(self): #normal case, no error
+        fakeFile = io.StringIO("""0 @I1@ INDI
+1 NAME Earl /Grey/
+1 SEX M
+1 FAMC @F2@
+1 FAMS @F1@
+0 @I2@ INDI
+1 NAME May /O'naise/
+1 SEX F
+1 FAMC @F3@
+1 FAMS @F1@
+0 @F1@ FAM
+1 HUSB @I1@
+1 WIFE @I2@
+1 MARR
+2 DATE 11 MAY 1960
+0 @F2@ FAM
+1 CHIL @I1@
+0 @F3@ FAM
+1 CHIL @I2@
+3 NOTE The above will show no error, because they are not siblings""")
+        fakeFile.seek(0)
+        parser = GEDCOMParser(fakeFile)
+        parser.extract_entities() #get individuals and families
+        parser.checkSiblingMarriage()
+        self.assertEqual(parser.errorStr, "") #function worked if errorStr is still empty
+
+    def test_marriage_of_siblings(self): #error case, siblings
+        fakeFile = io.StringIO("""0 @I1@ INDI
+1 NAME Earl /Grey/
+1 SEX M
+1 FAMC @F2@
+1 FAMS @F1@
+0 @I2@ INDI
+1 NAME May /O'naise/
+1 SEX F
+1 FAMC @F2@
+1 FAMS @F1@
+0 @F1@ FAM
+1 HUSB @I1@
+1 WIFE @I2@
+1 MARR
+2 DATE 11 MAY 1960
+0 @F2@ FAM
+1 CHIL @I1@
+1 CHIL @I2@
+3 NOTE The above will show no error, because they are not siblings""")
+        fakeFile.seek(0)
+        parser = GEDCOMParser(fakeFile)
+        parser.extract_entities() #get individuals and families
+        parser.checkSiblingMarriage()
+        self.assertNotEqual(parser.errorStr, "") #They are siblings, they are not valid to be married
+
+if __name__ == '__main__':
+    unittest.main()
